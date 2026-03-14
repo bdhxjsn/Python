685fb2ba6cd1827e85b51d04116770444987d0f7
lol <loliamnot512@gmail.com>
2026-03-14 14:48:37 +0530

    Conditional O/P - 1 script

---
+++ 016_conditional_operators.py
@@ -0,0 +1,10 @@
+_age = int(input("Kindly, Enter you age --> "))
+
+if _age < 18 and _age > 0 : #Here 'and' is OPERATOR
+    print("!NOT__ALLOWED!")
+elif _age < 0 :
+    print("INVALID \'Skill issue lil, bro.\'")
+elif _age == 0 :
+    print("I think you are not BORN, yet!!")
+else :
+    print("$ALLOWED$")
